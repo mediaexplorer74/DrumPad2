bb9b9320403dd808e52f191782a064aac48d3b2a
MediaExplorer <mediaexplorer74@hotmail.com>
2021-09-05 18:05:22 +0300

    .

---
+++ DrumPad/DrumPad/App.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Utilities;
+using Xamarin.Forms;
+
+namespace DrumPad
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new NavigationPage(new DrumPad.MainPage());
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+
+        public static void UpdateThemeColors (ColorScheme scheme)
+        {
+            Current.Resources["mainColor"] = XFUtilities.GetColorFromInt(scheme.MainColor);
+            Current.Resources["highlightColor"] = XFUtilities.GetColorFromInt(scheme.HighlightColor);
+            Current.Resources["buttonColor"] = XFUtilities.GetColorFromInt(scheme.ButtonColor);
+            Current.Resources["backgroundColor"] = XFUtilities.GetColorFromInt(scheme.BackgroundColor);
+        }
+    }
+}
---
+++ readme.md
@@ -0,0 +1,19 @@
+# Xamarin.Forms DrumPad-2 (Druminator 3001)
+
+## DrumPad2 Sample App (Android, iOS, UWP, WPF)
+This is a lite remix of the sample DrumPad app built for the "Xamarin Webinar Series" (Xamarin University).
+
+![Druminator 3001](https://github.com/mediaexplorer74/DrumPad2/blob/main/shot.jpg)
+
+This version is more complete, then DrumPad sample. 
+It contains more features, and makes use of the SimpleAudioPlayer nuget package:
+https://www.nuget.org/packages/Xam.Plugin.SimpleAudioPlayer/
+
+Includes:
+- themes
+- animations
+- audio playback
+
+UWMP - Minimal Req.: Win10Mobile os build 15063
+
+P.S. This mini-project maded for main RnD projects "Bochs" and "x86onARM" (theme: use multiplatform features and transfer "pure x86" code to "UWP kernel")
